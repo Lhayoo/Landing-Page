5336dcfdc4d0c02fa5d05da54539362ada366935
M. Nurul Alam <m.nurulalam200@gmail.com>
2022-08-04 11:35:25 +0700

    javaScript

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// function getIcon
+let getIcon = function (id) {
+    return document.getElementsByClassName(icon);
+}
+// function change Icon
+let changeIcon = function (id) {
+    id.classList.toggle('fa-xmark');
+}
+
+
+
