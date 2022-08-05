2fe05ae3c76eeca8eb6ce52ade3f54e2bca0aea4
M. Nurul Alam <m.nurulalam200@gmail.com>
2022-08-05 10:12:01 +0700

    scrolling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,24 @@
 // function getIcon
 let getIcon = function (id) {
-    return document.getElementsByClassName(icon);
+    return document.getElementsByName(check);
 }
 // function change Icon
 let changeIcon = function (id) {
     id.classList.toggle('fa-xmark');
 }
 
+// scroll animation
+const card = document.querySelectorAll(".card");
 
+const checkCard = () => {
+    const triggerBotttom = (window.innerHeight / 5) * 4;
+    card.forEach((card) => {
+        const cardTop = card.getBoundingClientRect().top;
+        if (cardTop < triggerBotttom) card.classList.add("show");
+        else card.classList.remove("show");
+    });
+};
+
+window.addEventListener("scroll", checkCard);
+checkCard()
 
